6facadcef6a43c3bd175c6b6ffc6aba7d56ee596
sadmann7 <sadmann898@gmail.com>
2024-11-29 22:18:44 +0600

    feat: check rtl

--- packages/tags-input/src/tags-input-clear.tsx
+++ packages/tags-input/src/tags-input-clear.tsx
@@ -1,3 +1,4 @@
+import { composeEventHandlers } from "@diceui/shared";
 import { Primitive } from "@radix-ui/react-primitive";
 import * as React from "react";
 import { useTagsInput } from "./tags-input-root";
@@ -9,19 +10,17 @@ const TagsInputClear = React.forwardRef<HTMLButtonElement, TagsInputClearProps>(
   (props, ref) => {
     const context = useTagsInput();
 
-    function onClear() {
-      if (context.disabled) return;
-      context.onValuesChange([]);
-      context.inputRef.current?.focus();
-    }
-
     return (
       <Primitive.button
         ref={ref}
         type="button"
         data-state={context.values.length > 0 ? "visible" : "hidden"}
         data-disabled={context.disabled ? "" : undefined}
-        onClick={onClear}
+        onClick={composeEventHandlers(props.onClick, () => {
+          if (context.disabled) return;
+          context.onValuesChange([]);
+          context.inputRef.current?.focus();
+        })}
         {...props}
       />
     );
