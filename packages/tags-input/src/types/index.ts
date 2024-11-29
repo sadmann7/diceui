def7655c62250b8329803f2177d1113ae4605a06
sadmann7 <sadmann898@gmail.com>
2024-11-29 14:54:07 +0600

    chore: fix lint

--- packages/tags-input/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export type InputValue = string | Record<string, string>;
-
-export interface TagItem {
-  id: string;
-  value: string;
-}
-
-export type TagValue<T> = T extends string ? T | TagItem : T;
