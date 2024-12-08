a72f817b71111052c17ada69842e0e4b25825a23
sadmann7 <sadmann898@gmail.com>
2024-12-08 21:32:15 +0600

    feat: update code-block

--- docs/components/code-block-wrapper.tsx
+++ docs/components/code-block-wrapper.tsx
@@ -14,7 +14,8 @@ import {
 } from "@/components/ui/collapsible";
 import { cn } from "@/lib/utils";
 
-interface CodeBlockProps extends React.HTMLAttributes<HTMLDivElement> {
+interface CodeBlockProps
+  extends React.ComponentPropsWithoutRef<typeof Collapsible> {
   expandButtonTitle?: string;
 }
 
@@ -27,33 +28,36 @@ export function CodeBlockWrapper({
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Collapsible open={open} onOpenChange={setOpen}>
-      <div className={cn("relative overflow-hidden", className)} {...props}>
-        <CollapsibleContent
-          forceMount
-          className={cn("overflow-hidden", !open && "max-h-32")}
-        >
-          <div
-            className={cn(
-              "[&_pre]:my-0 [&_pre]:max-h-[650px] [&_pre]:pb-[100px]",
-              !open ? "[&_pre]:overflow-hidden" : "[&_pre]:overflow-auto]",
-            )}
-          >
-            {children}
-          </div>
-        </CollapsibleContent>
+    <Collapsible
+      open={open}
+      onOpenChange={setOpen}
+      className={cn("relative overflow-hidden", className)}
+      {...props}
+    >
+      <CollapsibleContent
+        forceMount
+        className={cn("overflow-hidden", !open && "max-h-32")}
+      >
         <div
           className={cn(
-            "absolute flex items-center justify-center bg-gradient-to-b from-zinc-700/30 to-zinc-950/90 p-2",
-            open ? "inset-x-0 bottom-0 h-12" : "inset-0",
+            "[&_figure]:my-0 [&_figure]:max-h-[640px] [&_figure]:pb-[20px]",
+            !open ? "[&_figure]:overflow-hidden" : "[&_figure]:overflow-auto]",
           )}
         >
-          <CollapsibleTrigger asChild>
-            <Button variant="secondary" className="h-8 text-xs">
-              {open ? "Collapse" : expandButtonTitle}
-            </Button>
-          </CollapsibleTrigger>
+          {children}
         </div>
+      </CollapsibleContent>
+      <div
+        className={cn(
+          "absolute flex items-center justify-center bg-gradient-to-b from-zinc-700/30 to-zinc-950/90 p-2",
+          open ? "inset-x-0 bottom-0 h-12" : "inset-0",
+        )}
+      >
+        <CollapsibleTrigger asChild>
+          <Button className="h-[2.1rem] bg-background px-3 text-foreground hover:bg-background/70">
+            {open ? "Collapse" : expandButtonTitle} code
+          </Button>
+        </CollapsibleTrigger>
       </div>
     </Collapsible>
   );
