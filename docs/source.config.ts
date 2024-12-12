c62b1c9049845450bf9a82bca64347e4fb8544fd
sadmann7 <sadmann898@gmail.com>
2024-12-12 21:52:52 +0600

    feat: update list

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -65,6 +65,7 @@ export const { docs, meta } = defineDocs({
   dir: "content/docs",
   docs: {
     schema: frontmatterSchema.extend({
+      preview: z.boolean().optional(),
       links: z
         .object({
           doc: z.string().optional(),
