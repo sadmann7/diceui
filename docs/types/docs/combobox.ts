5eebf2cf28d48fde638d09e42d8461cf10b6d7dc
sadmann7 <sadmann898@gmail.com>
2024-12-24 01:21:45 +0600

    feat: add combobox docs

---
+++ docs/types/docs/combobox.ts
@@ -0,0 +1,99 @@
+import type {
+  ComboboxAnchorProps,
+  ComboboxArrowProps,
+  ComboboxCancelProps,
+  ComboboxContentProps,
+  ComboboxEmptyProps,
+  ComboboxGroupLabelProps,
+  ComboboxGroupProps,
+  ComboboxInputProps,
+  ComboboxItemIndicatorProps,
+  ComboboxItemProps,
+  ComboboxItemTextProps,
+  ComboboxLabelProps,
+  ComboboxPortalProps,
+  ComboboxRootProps,
+  ComboboxSeparatorProps,
+  ComboboxTriggerProps,
+} from "@diceui/combobox";
+import type * as React from "react";
+
+export type RootProps<Multiple extends boolean = false> = Omit<
+  ComboboxRootProps<Multiple>,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type LabelProps = Omit<
+  ComboboxLabelProps,
+  keyof React.ComponentPropsWithoutRef<"label">
+>;
+
+export type AnchorProps = Omit<
+  ComboboxAnchorProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type TriggerProps = Omit<
+  ComboboxTriggerProps,
+  keyof React.ComponentPropsWithoutRef<"button">
+>;
+
+export type InputProps = Omit<
+  ComboboxInputProps,
+  keyof React.ComponentPropsWithoutRef<"input">
+>;
+
+export type CancelProps = Omit<
+  ComboboxCancelProps,
+  keyof React.ComponentPropsWithoutRef<"button">
+>;
+
+export type PortalProps = Omit<
+  ComboboxPortalProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type ContentProps = Omit<
+  ComboboxContentProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type ArrowProps = Omit<
+  ComboboxArrowProps,
+  keyof React.ComponentPropsWithoutRef<"svg">
+>;
+
+export type EmptyProps = Omit<
+  ComboboxEmptyProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type GroupProps = Omit<
+  ComboboxGroupProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type GroupLabelProps = Omit<
+  ComboboxGroupLabelProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type ItemProps = Omit<
+  ComboboxItemProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
+
+export type ItemTextProps = Omit<
+  ComboboxItemTextProps,
+  keyof React.ComponentPropsWithoutRef<"span">
+>;
+
+export type ItemIndicatorProps = Omit<
+  ComboboxItemIndicatorProps,
+  keyof React.ComponentPropsWithoutRef<"span">
+>;
+
+export type SeparatorProps = Omit<
+  ComboboxSeparatorProps,
+  keyof React.ComponentPropsWithoutRef<"div">
+>;
--- packages/combobox/src/index.ts
+++ packages/combobox/src/index.ts
@@ -4,17 +4,32 @@ export type { ComboboxRootProps } from "./combobox-root";
 export { Anchor, ComboboxAnchor } from "./combobox-anchor";
 export type { ComboboxAnchorProps } from "./combobox-anchor";
 
+export { ComboboxTrigger, Trigger } from "./combobox-trigger";
+export type { ComboboxTriggerProps } from "./combobox-trigger";
+
 export { Arrow, ComboboxArrow } from "./combobox-arrow";
 export type { ComboboxArrowProps } from "./combobox-arrow";
 
+export { ComboboxCancel, Cancel } from "./combobox-cancel";
+export type { ComboboxCancelProps } from "./combobox-cancel";
+
+export { ComboboxPortal, Portal } from "./combobox-portal";
+export type { ComboboxPortalProps } from "./combobox-portal";
+
 export { ComboboxContent, Content } from "./combobox-content";
 export type { ComboboxContentProps } from "./combobox-content";
+
+export { ComboboxLabel, Label } from "./combobox-label";
+export type { ComboboxLabelProps } from "./combobox-label";
 
 export { ComboboxEmpty, Empty } from "./combobox-empty";
 export type { ComboboxEmptyProps } from "./combobox-empty";
 
 export { ComboboxGroup, Group } from "./combobox-group";
 export type { ComboboxGroupProps } from "./combobox-group";
+
+export { ComboboxGroupLabel, GroupLabel } from "./combobox-group-label";
+export type { ComboboxGroupLabelProps } from "./combobox-group-label";
 
 export { ComboboxInput, Input } from "./combobox-input";
 export type { ComboboxInputProps } from "./combobox-input";
@@ -31,11 +46,5 @@ export type { ComboboxItemIndicatorProps } from "./combobox-item-indicator";
 export { ComboboxItemText, ItemText } from "./combobox-item-text";
 export type { ComboboxItemTextProps } from "./combobox-item-text";
 
-export { ComboboxPortal, Portal } from "./combobox-portal";
-export type { ComboboxPortalProps } from "./combobox-portal";
-
 export { ComboboxSeparator, Separator } from "./combobox-separator";
 export type { ComboboxSeparatorProps } from "./combobox-separator";
-
-export { ComboboxTrigger, Trigger } from "./combobox-trigger";
-export type { ComboboxTriggerProps } from "./combobox-trigger";
