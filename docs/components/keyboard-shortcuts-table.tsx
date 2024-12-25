d9cf8f3e8fc090802d4a07f30f659c754eed7baa
sadmann7 <sadmann898@gmail.com>
2024-12-25 20:57:34 +0600

    feat: update keyboard table

--- docs/components/keyboard-shortcuts-table.tsx
+++ docs/components/keyboard-shortcuts-table.tsx
@@ -12,7 +12,6 @@ interface KeyboardShortcutsTableProps {
   shortcuts: {
     keys: string[];
     description: string;
-    combined?: boolean;
   }[];
 }
 
@@ -31,15 +30,11 @@ export function KeyboardShortcutsTable({
         {shortcuts.map((shortcut, index) => (
           <TableRow key={`${shortcut.keys.join(" + ")}-${index}`}>
             <TableCell>
-              {shortcut.combined ? (
-                <Kbd variant="outline">{shortcut.keys.join(" + ")}</Kbd>
-              ) : (
-                shortcut.keys.map((key) => (
-                  <Kbd key={key} variant="outline" className="mr-2">
-                    {key}
-                  </Kbd>
-                ))
-              )}
+              {shortcut.keys.map((key) => (
+                <Kbd key={key} variant="outline" className="mr-2">
+                  {key}
+                </Kbd>
+              ))}
             </TableCell>
             <TableCell>
               <span>{shortcut.description}</span>
