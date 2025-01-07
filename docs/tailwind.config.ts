6613675393595a780e1c11f25f55acf5f870392b
sadmann7 <sadmann898@gmail.com>
2025-01-07 11:27:21 +0600

    chore: bump deps

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -4,7 +4,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: ["class"],
-  presets: [createPreset()],
+  presets: [createPreset({ preset: "black" })],
   content: [
     "./node_modules/fumadocs-ui/dist/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
