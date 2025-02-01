3daf81586cc080fb29177bc2d516b42f7f83776d
sadmann7 <sadmann898@gmail.com>
2025-02-01 23:29:50 +0600

    chore: fix build error

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
