90d04622e6ca77cc6ea20601af3343e6b35dd665
sadmann7 <sadmann898@gmail.com>
2025-02-09 06:27:07 +0600

    chore: bump deps

---
+++ .changeset/rotten-houses-ring.md
@@ -0,0 +1,9 @@
+---
+"@diceui/checkbox-group": minor
+"@diceui/tags-input": minor
+"@diceui/combobox": minor
+"@diceui/mention": minor
+"@diceui/shared": minor
+---
+
+Bump `@floating-ui/react`
--- examples/nextjs/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Already doing linting and typechecking as separate tasks in CI
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-
-export default nextConfig;
