cca72342e5d8f4e3338a627c698969450a827ebc
Sadman Sakib <68962712+sadmann7@users.noreply.github.com>
2025-03-02 22:53:06 +0600

    feat: add og (#28)
    * feat: add og
    
    * chore: update robots.ts

--- docs/app/(lobby)/page.tsx
+++ docs/app/(lobby)/page.tsx
@@ -6,15 +6,15 @@ import { siteConfig } from "@/config/site";
 export default function IndexPage() {
   return (
     <section className="container flex flex-col items-center justify-center gap-6 pt-6 pb-8 md:py-10">
-      <div className="max-w-5xl space-y-8">
+      <div className="flex max-w-5xl flex-col gap-8">
         <h1
-          className="animate-fade-up text-balance bg-linear-to-br from-foreground/80 to-muted-foreground bg-clip-text text-center font-bold text-5xl/[3rem] text-transparent opacity-0 drop-shadow-xs md:text-7xl/[5rem]"
+          className="animate-fade-up text-balance bg-linear-to-br from-foreground/80 to-muted-foreground bg-clip-text text-center font-bold text-5xl/[3rem] text-transparent leading-tight tracking-tighter opacity-0 drop-shadow-xs md:text-7xl/[5rem]"
           style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
         >
           {siteConfig.name}
         </h1>
         <p
-          className="animate-fade-up text-balance text-center text-muted-foreground/80 opacity-0 md:text-xl"
+          className="max-w-2xl animate-fade-up text-balance text-center font-light text-lg text-muted-foreground opacity-0 md:text-xl"
           style={{ animationDelay: "0.30s", animationFillMode: "forwards" }}
         >
           {siteConfig.description}
