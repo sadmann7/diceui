79188197b4b28aa66d38f3f93505e11033b73c3a
sadmann7 <sadmann898@gmail.com>
2025-03-29 03:16:34 +0600

    feat: add data-table credits

--- docs/components/data-table-filter-list.tsx
+++ docs/components/data-table-filter-list.tsx
@@ -48,6 +48,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Sortable,
+  SortableContent,
+  SortableItem,
+  SortableItemHandle,
+  SortableOverlay,
+} from "@/components/ui/sortable";
 import { dataTableConfig } from "@/config/data-table";
 import { useDebouncedCallback } from "@/hooks/use-debounced-callback";
 import { getDefaultFilterOperator, getFilterOperators } from "@/lib/data-table";
@@ -55,13 +62,6 @@ import { formatDate } from "@/lib/format";
 import { generateId } from "@/lib/id";
 import { getFiltersStateParser } from "@/lib/parsers";
 import { cn } from "@/lib/utils";
-import {
-  Sortable,
-  SortableContent,
-  SortableItem,
-  SortableItemHandle,
-  SortableOverlay,
-} from "@/registry/default/ui/sortable";
 import type {
   ExtendedColumnFilter,
   FilterOperator,
