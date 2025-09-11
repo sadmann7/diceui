224d3ffd9237e380c933ecd185225457b18d3869
sadmann7 <sadmann898@gmail.com>
2025-09-12 00:22:09 +0600

    feat: update stepper

--- docs/__registry__/index.tsx
+++ docs/__registry__/index.tsx
@@ -293,10 +293,6 @@ export const Index: Record<string, any> = {
         type: "registry:ui",
         target: ""
       },{
-        path: "registry/default/components/visually-hidden-input.tsx",
-        type: "registry:component",
-        target: ""
-      },{
         path: "registry/default/lib/compose-refs.ts",
         type: "registry:lib",
         target: ""
