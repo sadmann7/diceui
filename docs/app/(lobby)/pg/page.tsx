6cc3a0076ca80a004257081f6dc9d7c922548238
sadmann7 <sadmann898@gmail.com>
2025-09-14 10:26:40 +0600

    chore: clenaup

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -27,7 +27,6 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { tricks } from "@/lib/data";
-import EditableFormDemo from "@/registry/default/examples/editable-form-demo";
 import MaskInputDemo from "@/registry/default/examples/mask-input-demo";
 import {
   Combobox,
@@ -42,7 +41,6 @@ import {
 export default function PlaygroundPage() {
   return (
     <Shell>
-      <EditableFormDemo />
       <MaskInputDemo />
       <Combobox className="w-[15rem]">
         <ComboboxAnchor>
