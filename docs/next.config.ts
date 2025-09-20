4d4116e2ed28122962111973118f1f63210a7fb7
sadmann7 <sadmann898@gmail.com>
2025-09-20 16:17:15 +0600

    chore: sync

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -55,8 +55,8 @@ const nextConfig: NextConfig = {
     ];
   },
   // Already doing linting and typechecking as separate tasks in CI
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  // eslint: { ignoreDuringBuilds: true },
+  // typescript: { ignoreBuildErrors: true },
 };
 
 export default withMDX(nextConfig);
