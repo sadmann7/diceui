9080f2d7d3bab3095cf0bbc7f03ebe62acc09c88
sadmann7 <sadmann898@gmail.com>
2025-10-28 01:01:55 +0600

    test: cleanup agin

--- docs/registry/default/test/mask-input.test.tsx
+++ docs/registry/default/test/mask-input.test.tsx
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom";
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import * as React from "react";
