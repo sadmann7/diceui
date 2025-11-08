1f78b6b538017a524635e0ae206bdc4ade00413c
sadmann7 <sadmann898@gmail.com>
2025-11-08 09:35:49 +0600

    chore: update pg

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,18 +1,12 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import ScrollSpyControlledDemo from "@/registry/default/examples/scroll-spy-controlled-demo";
-import ScrollSpyDemo from "@/registry/default/examples/scroll-spy-demo";
-import ScrollSpyVerticalDemo from "@/registry/default/examples/scroll-spy-vertical-demo";
 import TourDemo from "@/registry/default/examples/tour-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <ScrollSpyDemo />
-        <ScrollSpyControlledDemo />
-        <ScrollSpyVerticalDemo />
         <TourDemo />
         <ColorPickerDemo />
       </Demo>
