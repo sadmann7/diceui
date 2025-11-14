e085f70d22e655eeb0e12da4d8821f8a5f965dab
sadmann7 <sadmann898@gmail.com>
2025-11-14 23:31:23 +0600

    feat: update page

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -4,17 +4,13 @@ import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
 import ScrollSpyControlledDemo from "@/registry/default/examples/scroll-spy-controlled-demo";
 import ScrollSpyDemo from "@/registry/default/examples/scroll-spy-demo";
 import ScrollSpyVerticalDemo from "@/registry/default/examples/scroll-spy-vertical-demo";
-import StackCustomDemo from "@/registry/default/examples/stack-custom-demo";
 import StackDemo from "@/registry/default/examples/stack-demo";
-import StackNoExpandDemo from "@/registry/default/examples/stack-no-expand-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
         <StackDemo />
-        <StackCustomDemo />
-        <StackNoExpandDemo />
         <ScrollSpyDemo />
         <ScrollSpyControlledDemo />
         <ScrollSpyVerticalDemo />
