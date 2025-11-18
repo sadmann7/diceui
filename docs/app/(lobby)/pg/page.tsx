8c6ed43337293f7d9f54106cc6c1fdd769c8344b
sadmann7 <sadmann898@gmail.com>
2025-11-18 16:48:26 +0600

    chore: rebuild registry

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,13 +1,11 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import TourControlledDemo from "@/registry/default/examples/tour-controlled-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <TourControlledDemo />
         <ColorPickerDemo />
       </Demo>
     </Shell>
