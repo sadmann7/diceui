0648fd084fb86d17ade0d4a7dcab68fd9534570b
sadmann7 <sadmann898@gmail.com>
2025-11-18 16:04:55 +0600

    chore: cleanup page

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,15 +1,11 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import FpsDemo from "@/registry/default/examples/fps-demo";
-import FpsStrategyDemo from "@/registry/default/examples/fps-strategy-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <FpsDemo />
-        <FpsStrategyDemo />
         <ColorPickerDemo />
       </Demo>
     </Shell>
