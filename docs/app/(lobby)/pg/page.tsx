78f65d896a55ec201f9df8776542e5bd811c9a92
sadmann7 <sadmann898@gmail.com>
2025-11-19 22:25:35 +0600

    chore: cleanup playground

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -2,12 +2,14 @@ import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
 import TimelineDemo from "@/registry/default/examples/timeline-demo";
+import TimelineHorizontalDemo from "@/registry/default/examples/timeline-horizontal-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
         <TimelineDemo />
+        <TimelineHorizontalDemo />
         <ColorPickerDemo />
       </Demo>
     </Shell>
