9106f3764cc5b968584b0b918f5423f70396915a
sadmann7 <sadmann898@gmail.com>
2025-11-24 02:35:32 +0600

    chore: update page

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,12 +1,7 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import TimePicker12HourDemo from "@/registry/default/examples/time-picker-12hour-demo";
-import TimePickerControlledDemo from "@/registry/default/examples/time-picker-controlled-demo";
 import TimePickerDemo from "@/registry/default/examples/time-picker-demo";
-import TimePickerFormDemo from "@/registry/default/examples/time-picker-form-demo";
-import TimePickerSecondsDemo from "@/registry/default/examples/time-picker-seconds-demo";
-import TimePickerStepDemo from "@/registry/default/examples/time-picker-step-demo";
 
 export default function PlaygroundPage() {
   return (
@@ -15,11 +10,6 @@ export default function PlaygroundPage() {
         <div className="flex flex-col gap-4">
           <input type="time" />
           <TimePickerDemo />
-          <TimePickerSecondsDemo />
-          <TimePicker12HourDemo />
-          <TimePickerFormDemo />
-          <TimePickerStepDemo />
-          <TimePickerControlledDemo />
         </div>
         <ColorPickerDemo />
       </Demo>
