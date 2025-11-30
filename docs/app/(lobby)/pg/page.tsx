74160eda34a1fd52b171038de2035a4e987d7e5d
sadmann7 <sadmann898@gmail.com>
2025-11-30 22:49:11 +0600

    chore: cleanup

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -3,13 +3,11 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import KeyValueDemo from "@/registry/default/examples/key-value-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <KeyValueDemo />
         <ColorPickerDemo />
       </Demo>
     </Shell>
