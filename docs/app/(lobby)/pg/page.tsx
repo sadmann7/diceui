54d1cc9789b5089d5a6ca9750158efe6f9f99c55
sadmann7 <sadmann898@gmail.com>
2025-12-01 00:30:13 +0600

    feat: add stat

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -3,11 +3,13 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
+import StatDemo from "@/registry/default/examples/stat-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
+        <StatDemo />
         <ColorPickerDemo />
       </Demo>
     </Shell>
