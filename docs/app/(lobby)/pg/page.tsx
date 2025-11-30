861054c77b29ac876f8ed15924861570cd49864a
sadmann7 <sadmann898@gmail.com>
2025-11-30 21:08:08 +0600

    feat: add data-attributes

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -4,13 +4,11 @@ import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
 import KeyValueDemo from "@/registry/default/examples/key-value-demo";
-import TimePickerDemo from "@/registry/default/examples/time-picker-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <TimePickerDemo />
         <KeyValueDemo />
         <ColorPickerDemo />
       </Demo>
