f8a57c09c45d7c47d4b2aabfcdc99b71eeb8249f
sadmann7 <sadmann898@gmail.com>
2025-12-04 22:12:38 +0600

    chore: cleanup pg

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -2,13 +2,6 @@
 
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
 import TimePickerDemo from "@/registry/default/examples/time-picker-demo";
 
@@ -17,16 +10,6 @@ export default function PlaygroundPage() {
     <Shell>
       <Demo>
         <div className="flex flex-col gap-4">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button>Select a time</Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent loop>
-              <DropdownMenuItem>09:00</DropdownMenuItem>
-              <DropdownMenuItem>10:00</DropdownMenuItem>
-              <DropdownMenuItem>11:00</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
           <input type="time" />
           <TimePickerDemo />
         </div>
