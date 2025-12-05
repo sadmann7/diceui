167ce9dbdf0d5cfcb8b07bd98751b96b7fbf826a
sadmann7 <sadmann898@gmail.com>
2025-12-05 22:47:54 +0600

    chore: cleanup compos

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -2,7 +2,6 @@
 
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
-import ActionBarDemo from "@/registry/default/examples/action-bar-demo";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
 import TimePickerDemo from "@/registry/default/examples/time-picker-demo";
 
@@ -10,7 +9,6 @@ export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <ActionBarDemo />
         <div className="flex flex-col gap-4">
           <input type="time" />
           <TimePickerDemo />
