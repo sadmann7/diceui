5b43218765fe4129c4a13a6e6bf52b9667b438d0
sadmann7 <sadmann898@gmail.com>
2025-12-09 09:56:43 +0600

    feat: update playground

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -3,16 +3,12 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import CropperDemo from "@/registry/default/examples/cropper-demo";
 import SpeedDialDemo from "@/registry/default/examples/speed-dial-demo";
-import StepperDemo from "@/registry/default/examples/stepper-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <CropperDemo />
-        <StepperDemo />
         <SpeedDialDemo />
         <ColorPickerDemo />
       </Demo>
