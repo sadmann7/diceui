83faa918942fa7f144d80083be24128041591aae
sadmann7 <sadmann898@gmail.com>
2025-12-22 18:17:00 +0600

    chore: cleanup playground again

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,8 +1,6 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import GaugeDemo from "@/registry/default/examples/gauge-demo";
-import GaugeVariantsDemo from "@/registry/default/examples/gauge-variants-demo";
 import SpeedDialControlledDemo from "@/registry/default/examples/speed-dial-controlled-demo";
 import SpeedDialDemo from "@/registry/default/examples/speed-dial-demo";
 import SpeedDialSideDemo from "@/registry/default/examples/speed-dial-side-demo";
@@ -11,8 +9,6 @@ export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <GaugeDemo />
-        <GaugeVariantsDemo />
         <SpeedDialDemo />
         <SpeedDialControlledDemo />
         <SpeedDialSideDemo />
