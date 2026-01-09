f03df1ccd31ff933ff192145e85b4dc40987aba3
sadmann7 <sadmann898@gmail.com>
2026-01-09 20:18:13 +0600

    chore: update pg

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,5 +1,6 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
+import { Button } from "@/components/ui/button";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
 import SelectionToolbarDemo from "@/registry/default/examples/selection-toolbar-demo";
 import SelectionToolbarInfoDemo from "@/registry/default/examples/selection-toolbar-info-demo";
@@ -9,6 +10,7 @@ export default function PlaygroundPage() {
     <Shell>
       <Demo>
         <SelectionToolbarDemo />
+        <Button>Do a kickflip</Button>
         <SelectionToolbarInfoDemo />
         <ColorPickerDemo />
       </Demo>
