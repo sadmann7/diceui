fd9944c80d58fb48c08b4a1c1b23aa2de6f55f0f
sadmann7 <sadmann898@gmail.com>
2026-01-10 21:22:46 +0600

    chore: cleanup

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -1,21 +1,11 @@
 import { Demo } from "@/components/demo";
 import { Shell } from "@/components/shell";
-import { Button } from "@/components/ui/button";
 import ColorPickerDemo from "@/registry/default/examples/color-picker-demo";
-import SelectionToolbarDemo from "@/registry/default/examples/selection-toolbar-demo";
-import SelectionToolbarInfoDemo from "@/registry/default/examples/selection-toolbar-info-demo";
-import SwapAnimationsDemo from "@/registry/default/examples/swap-animations-demo";
-import SwapDemo from "@/registry/default/examples/swap-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <SwapDemo />
-        <SwapAnimationsDemo />
-        <SelectionToolbarDemo />
-        <Button>Do a kickflip</Button>
-        <SelectionToolbarInfoDemo />
         <ColorPickerDemo />
       </Demo>
     </Shell>
