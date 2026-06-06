316489e9144a3b842db6a54948cb65d12214e189
sadmann7 <sadmann898@gmail.com>
2026-06-06 21:10:51 +0600

    refactor: cleanup playground

--- docs/app/(lobby)/pg/page.tsx
+++ docs/app/(lobby)/pg/page.tsx
@@ -3,19 +3,13 @@ import { Shell } from "@/components/shell";
 import BannerBaseDemo from "@/registry/bases/base/examples/banner-demo";
 import BannerBaseStackedDemo from "@/registry/bases/base/examples/banner-stacked-demo";
 import ColorPickerDemo from "@/registry/bases/base/examples/color-picker-demo";
-import TimePickerBaseDemo from "@/registry/bases/base/examples/time-picker-demo";
 import BannerRadixDemo from "@/registry/bases/radix/examples/banner-demo";
 import BannerRadixStackedDemo from "@/registry/bases/radix/examples/banner-stacked-demo";
-import TimePickerRadixDemo from "@/registry/bases/radix/examples/time-picker-demo";
 
 export default function PlaygroundPage() {
   return (
     <Shell>
       <Demo>
-        <DemoItemGroup>
-          <TimePickerRadixDemo />
-          <TimePickerBaseDemo />
-        </DemoItemGroup>
         <DemoItemGroup>
           <BannerRadixDemo />
           <BannerBaseDemo />
