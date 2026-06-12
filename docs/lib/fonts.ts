aaea5e4dd35f8814e4aabd29f110cc12841655b0
Sadman Sakib <68962712+sadmann7@users.noreply.github.com>
2026-06-12 07:46:36 -0700

    chore(docs): fix build (#285)

--- docs/lib/fonts.ts
+++ docs/lib/fonts.ts
@@ -13,11 +13,8 @@ export const fontMono = Geist_Mono({
 export async function loadFonts(): Promise<
   { name: string; data: Buffer; weight: 400 | 600; style: "normal" }[]
 > {
-  const [normal, mono, semibold] = await Promise.all([
+  const [normal, semibold] = await Promise.all([
     import("../assets/fonts/geist-regular-otf.json").then(
-      (mod) => mod.default ?? mod,
-    ),
-    import("../assets/fonts/geistmono-regular-otf.json").then(
       (mod) => mod.default ?? mod,
     ),
     import("../assets/fonts/geist-semibold-otf.json").then(
@@ -29,12 +26,6 @@ export async function loadFonts(): Promise<
     {
       name: "Geist",
       data: Buffer.from(normal.base64Font, "base64"),
-      weight: 400 as const,
-      style: "normal" as const,
-    },
-    {
-      name: "Geist Mono",
-      data: Buffer.from(mono.base64Font, "base64"),
       weight: 400 as const,
       style: "normal" as const,
     },
